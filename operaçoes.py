43a31718b4ce776f1da2ff7fe03afdfbdca6525a
Roney <ronaldorhoney@hotmail.com>
2024-03-08 15:54:34 -0300

    subindo arquivos de Operações matemática

---
+++ operaçoes.py
@@ -0,0 +1,12 @@
+# Operações matemática
+
+faturamento = 1000
+custo = 700
+
+novas_vendas = 300
+faturamento = faturamento + novas_vendas
+lucro = faturamento - custo
+print(faturamento)
+print(lucro)
+margem_lucro = lucro / faturamento
+
